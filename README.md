6d28fdf2cc2b7adba3ceb03f35fc00a7b2000b7a
Jori Hiltunen <hiltusenjori@gmail.com>
2023-09-06 19:16:18 +0200

    Changes to be committed:     Add info and context to the project 	modified:   README.md
        Batch file to initiate react development environment
        new file:   startDevelopment.bat

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # open-react-dev-tools
+# 
+# 06.09.2023
+### Copyright Jori Hiltunen
+ hiltusenjori@gmail.com
+
+## General info
+Makes your life easier by opening code, git bash MINGW and starts npm
---
+++ startDevelopment.bat
@@ -0,0 +1,11 @@
+:: 
+:: Jorin react devaus-batch-script
+:: hiltusenjori@gmail.com
+:: Siirrä react-projektin juureen
+::
+
+@echo off
+
+start cmd /C "npm start"
+cmd /C code .
+"C:\Program Files\Git\bin\sh.exe"
